84b8f3537e0d56a8ad4a27b6596da2307c22abeb
antis0007 <31860133+antis0007@users.noreply.github.com>
2022-11-06 01:48:38 -0600

    WIP final functions

--- HackED Bot/discbot.py
+++ HackED Bot/discbot.py
@@ -10,7 +10,11 @@ from discord.ext import tasks, commands
 import uuid
 import requests
 import shutil
+import pickle
+import os
 
+#Custom Imports:
+import img2txt
 
 token = (open("token.txt")).readline() #Add your own token
 if(token == ""):
@@ -18,7 +22,18 @@ if(token == ""):
     input("Waiting for input to exit program...")
     quit()
 
-client = commands.Bot(command_prefix="//",intents=intents)
+client = commands.Bot(command_prefix=".",intents=intents)
+
+def load():
+    #open python pickle file within the same directory, if it doesnt exist, ensure it exists
+    try:
+        with open("usernames.pickle", "rb") as f:
+            usernames = pickle.load(f)
+    except FileNotFoundError:
+        with open("usernames.pickle", "wb") as f:
+            usernames = {}
+            pickle.dump(usernames, f)
+    return usernames
 
 @client.event
 
@@ -27,6 +42,7 @@ async def on_ready():
 
 @client.command()
 async def save(ctx):
+    userid = str(ctx.author.id)
     try:
         url = ctx.message.attachments[0].url
     except IndexError:
@@ -41,7 +57,102 @@ async def save(ctx):
                 print("Saving Image:" + imageName) 
                 await ctx.send("Image Saved!")
                 shutil.copyfileobj(req.raw, out_file)
+    cwd = os.getcwd()
+    #create path with os.path.join(
+    path = os.path.join(cwd, "imageName")
+    temp = img2txt.process(path)
+    usernames = load()
+    if not userid in usernames:
+        await ctx.send("You have not registered your username yet!")
+    else:
+        if usernames[userid][0] in temp:
+            kd = temp[usernames[userid][0]]
+            usernames[userid][1].append(kd)
+        else:
+            await ctx.send("Your username is not in this screenshot!")
+    #await saveusername(ctx, name)
 
+    
+    #print("Name of player is" , name)
+    #print("Kills:" , kd[0], "\nDeaths:", kd[1])
+
+
+
+@client.command()
+async def saveusername(ctx, query=""):
+    userid = str(ctx.author.id)
+    #associate discord id with username
+    if query == "":
+        await ctx.send("Please enter a username")
+    else:
+        #open python pickle file within the same directory, if it doesnt exist, ensure it exists
+        usernames = load()
+        #check if discord userid exists in dictionary, if not, add key
+        if str(ctx.author.id) not in usernames:
+            usernames[userid] = [query,[]]  #FORMAT: DiscordID: [Game Username, KDA History]
+            #kda history = [[kills,deaths], [kills,deaths], [kills,deaths]]
+            await ctx.send("Username saved!")
+            print(usernames[ctx.author.id])
+       
+        #save the dictionary with pickle
+        with open("usernames.pickle", "wb") as f:
+            pickle.dump(usernames, f)
+
+
+@client.command()
+async def updateusername(ctx, query=""):    
+    #updates the user's name in the dict
+    userid = str(ctx.author.id)
+    if query == "":
+        await ctx.send("Please enter a username")
+    else:
+        #open python pickle file within the same directory, if it doesnt exist, ensure it exists
+        usernames = load()
+        #check if discord userid exists in dictionary, if not, add key
+        if userid not in usernames:
+            await ctx.send("Nothing to update")
+        else:
+            usernames[userid][0] = query
+            await ctx.send("Username updated!")
+            print(usernames[userid][0])
+       
+        #save the dictionary with pickle
+        with open("usernames.pickle", "wb") as f:
+            pickle.dump(usernames, f)
+
+async def showleaderboard(ctx, query=""): 
+    #new function that calculates the score and spits it out as a leaderboard in an embed using the image parser amogh and andrew made
+    userid = str(ctx.author.id)
+    usernames = load()
+    if not userid in usernames:
+        await ctx.send("You have not registered your username yet!")
+    else:
+        averages = {}
+        for user in usernames:
+            if len(usernames[user][1]) > 0:
+                #averages[user] = (sum(usernames[user][1][0])/len(usernames[user][1][1]))
+                #averages[user]
+                kills = 0
+                deaths = 0
+                for i in range(len(usernames[user][1])): #iterate through history
+                    kills+= (usernames[user][1][i][0])
+                    deaths+= (usernames[user][1][i][0])
+                averages[user] = kills/deaths
+            else:
+                averages[user] = usernames[user][1][0][0]/usernames[user][1][0][1]
+            #generate top 10 leaderboard from greatest values in averages
+            leaderboard = sorted(averages.items(), key=lambda x: x[1], reverse=True)
+            leaderboard = leaderboard[:10]
+            #generate embed
+            embed = discord.Embed(title="Leaderboard", description="Top 10 Players", color=0xeee657)
+            for i in range(len(leaderboard)):
+                embed.add_field(name="Player - KDA", value=str(leaderboard[i][0]+" - "+leaderboard[i][1]), inline=True)
+
+        print(averages)
+
+        embed = discord.Embed(title="Leaderboard:",description=f"",colour=ctx.author.colour)
+        msg = await ctx.send(embed=embed)
+        
 
 client.run(token)
 
